f21760f38bf7c29d6d016d77eb1cef54349ff25a
Kameron H <kam.harvey10@gmail.com>
2025-05-14 19:10:31 -0400

    Initial Commit

---
+++ Todo2/Todo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo2App.swift
+//  Todo2
+//
+//  Created by Scholar on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todo2Tests/Todo2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo2Tests.swift
+//  Todo2Tests
+//
+//  Created by Scholar on 5/14/25.
+//
+
+import Testing
+@testable import Todo2
+
+struct Todo2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
