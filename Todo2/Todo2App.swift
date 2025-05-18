230f4e51235b8702cc6445bc4a6f8b88064b1840
Kameron H <kam.harvey10@gmail.com>
2025-05-17 20:03:42 -0400

    coded swift data

--- Todo2/Todo2App.swift
+++ Todo2/Todo2App.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 @main
 struct Todo2App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: ToDoItem.self)
         }
     }
 }
